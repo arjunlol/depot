c1e33dee0fb247ff4cefdb1e7e436a474660b189
Arjun Lall <lallarjun@gmail.com>
2017-11-18 23:29:08 -0500

    ajaxxx

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,4 @@
+
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
@@ -32,10 +33,13 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
-        format.json { render :show, status: :created, location: @line_item }
+        format.js
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
         format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -70,8 +74,10 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def line_item_params
       params.require(:line_item).permit(:product_id)
     end
+  #...
 end
